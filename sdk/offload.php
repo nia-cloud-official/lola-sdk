24d00b03a45717711b2a690331706f12090d47e3
Milton Vafana <146335315+nia-cloud-official@users.noreply.github.com>
2025-02-10 18:20:22 +0000

    Update offload.php

--- sdk/offload.php
+++ sdk/offload.php
@@ -6,12 +6,9 @@ class Controllers {}
 
 class DatabaseController extends Controllers
 {
-
     public $conn;
-
     public $allItems;
     public $current_filter;
-
     public $listofItems;
 
     public function __construct()
@@ -19,58 +16,72 @@ class DatabaseController extends Controllers
         $this->conn = $this->establishConnection();
     }
 
-    static function establishConnection()
+    private function establishConnection()
     {
         $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        if (!mysqli_connect_errno()) {
-            $status = "Successfully Connected to the database";
-        } else {
-            $status = "Failed to establish connection to" . DB_NAME . ' via ' . DB_HOST;
+        if (!$conn) {
+            $status = "Failed to establish connection to " . DB_NAME . ' via ' . DB_HOST;
             file_put_contents('connection_error_log.txt', $status);
+            die($status); // Handle the error appropriately
         }
         return $conn;
     }
 
-    // Collect all the stuff or list of things to search from
-
-    public function collectList()  
+    public function collectList()
     {
-        // By default the table is declared as 'LIST_TABLE' in the env.php file so you can change it there but for a more manual approach change it here.
-        // If you have already changed in the env.php file just uncomment the code below.
-        $table = 'blog';
-        /*$table = LIST_TABLE; */ 
+        $table = defined('LIST_TABLE') ? LIST_TABLE : 'blog';
         $selectQuery = "SELECT * FROM " . $table;
         $results = mysqli_query($this->conn, $selectQuery);
-        $result = mysqli_fetch_all($results);
-        $this->allItems = $result;
-        $this->listofItems = $result;
+        if ($results === false) {
+            die("Error executing query: " . mysqli_error($this->conn));
+        }
+        $this->allItems = mysqli_fetch_all($results, MYSQLI_ASSOC);
+        $this->listofItems = $this->allItems;
         return $this->listofItems;
     }
 
-    public function IsfilterApplied()
+    public function isFilterApplied()
     {
-        if(isset($_POST['type'])) {
+        if (isset($_POST['type'])) {
             $filter = $_POST['type'];
             $list_table = LIST_TABLE;
-            $FilterselectQuery = "SELECT * FROM $list_table WHERE `type` = '$filter'";
-            $results = mysqli_query($this->conn, $FilterselectQuery);
-            $result = mysqli_fetch_all($results);
-            $listofItems = $result;
-            $this->listofItems = $listofItems;
-            $this->current_filter = $_POST['type'];
-            return $this->listofItems;
-        }else{ 
-            // if no filter is applied ...it should just show everything
-            $this->collectList();
-            foreach($this->allItems as $item){ 
-                return $item;
+            $filterSelectQuery = "SELECT * FROM $list_table WHERE `type` = '$filter'";
+            $results = mysqli_query($this->conn, $filterSelectQuery);
+            if ($results === false) {
+                die("Error executing query: " . mysqli_error($this->conn));
             }
+            $this->listofItems = mysqli_fetch_all($results, MYSQLI_ASSOC);
+            $this->current_filter = $filter;
+        } else {
+            $this->collectList();
         }
+        return $this->listofItems;
+    }
+
+    public function addItem($data)
+    {
+        $table = defined('LIST_TABLE') ? LIST_TABLE : 'blog';
+        $columns = implode(", ", array_keys($data));
+        $values = implode("', '", array_values($data));
+        $insertQuery = "INSERT INTO $table ($columns) VALUES ('$values')";
+        if (!mysqli_query($this->conn, $insertQuery)) {
+            die("Error inserting data: " . mysqli_error($this->conn));
+        }
+        return true;
+    }
+
+    public function deleteItem($id)
+    {
+        $table = defined('LIST_TABLE') ? LIST_TABLE : 'blog';
+        $deleteQuery = "DELETE FROM $table WHERE id = $id";
+        if (!mysqli_query($this->conn, $deleteQuery)) {
+            die("Error deleting data: " . mysqli_error($this->conn));
+        }
+        return true;
     }
 }
 
-
+// Initialize database connection
 global $conn;
 $conn = new DatabaseController();
-$conn::establishConnection();
-
+$conn->establishConnection();
